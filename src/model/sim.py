11f5426fc7fb0639217a87b634662cda9880e3be
Bogdan Foca <81586357+Zeyt8@users.noreply.github.com>
2026-06-27 15:35:59 +0200

    Update sim.py

--- src/model/sim.py
+++ src/model/sim.py
@@ -29,7 +29,7 @@ def enable_legend_toggles(ax, lines):
 
     ax.figure.canvas.mpl_connect('pick_event', on_pick)
 
-def get_circuit(touch_nodes):
+def get_circuit(touch_nodes, r_ladder):
     circuit = Circuit(f'RL + LPF + Envelope @ {f} Hz')
 
     circuit.SinusoidalVoltageSource(
@@ -40,33 +40,29 @@ def get_circuit(touch_nodes):
         frequency=f@u_kHz
     )
 
-    #circuit.R('r1', 'vin', 'vhpf', 1@u_Ohm)
     circuit.R('lpf_r', 'vin', 'vlpf', 200@u_Ohm)
     circuit.C('lpf_c', 'vlpf', circuit.gnd, 100@u_pF)
 
     circuit.C('hpf_c', 'vlpf', 'vhpf', 4.7@u_nF)
     circuit.R('hpf_r', 'vhpf', circuit.gnd, 34.2@u_kOhm)
 
-    circuit.VCVS('amp', 'vamp', circuit.gnd, 'vhpf', circuit.gnd, 1.5)
+    circuit.VCVS('amp', 'vamp', circuit.gnd, 'vhpf', circuit.gnd, 2)
 
-    circuit.R(1, 'vamp', 'n1', 1000@u_kOhm)
+    circuit.R(1, 'vamp', 'n1', 440@u_kOhm)
     circuit.L("inductor", 'n1', 'd-pad0', 100@u_mH)
-    circuit.R("d-padR0", 'd-pad0', 'd-pad1', 800@u_kOhm)
-    circuit.R("d-padR1", 'd-pad1', 'd-pad2', 300@u_kOhm)
-    circuit.R("d-padR2", 'd-pad2', 'd-pad3', 200@u_kOhm)
-    circuit.R("d-padR3", 'd-pad3', 'd-pad4', 100@u_kOhm)
-    circuit.R("short", 'd-pad4', circuit.gnd, 100@u_MOhm)
+    circuit.R("d-padR0", 'd-pad0', 'd-pad1', r_ladder[0])
+    circuit.R("d-padR1", 'd-pad1', 'd-pad2', r_ladder[1])
+    circuit.R("d-padR2", 'd-pad2', 'd-pad3', r_ladder[2])
+    circuit.R("d-padR3", 'd-pad3', 'd-pad4', r_ladder[3])
+    circuit.R("r_pad4_gnd", 'd-pad4', circuit.gnd, 100@u_MOhm)
     for touch_node in touch_nodes:
         circuit.C(f"d-padC{touch_node}", f'd-pad{touch_node}', circuit.gnd, 100@u_pF)
 
     circuit.VCVS('buffer', 'vbuf', circuit.gnd, 'n1', circuit.gnd, 1)
 
-    #circuit.R('lpf2_r', 'vbuf', 'vlpf2', 1@u_Ohm)
     circuit.R('lpf2_r', 'vbuf', 'vlpf2', 200@u_Ohm)
     circuit.C('lpf2_c', 'vlpf2', circuit.gnd, 100@u_pF)
 
-
-    #circuit.R('env_r', 'vlpf2', 'venv', 1@u_Ohm)
     circuit.D('d1', 'vlpf2', 'venv', model='Dideal')
     circuit.R('env_r', 'venv', circuit.gnd, 10@u_kOhm)
     circuit.C('env_c', 'venv', circuit.gnd, 660@u_nF)
@@ -110,8 +106,8 @@ def plot_all(analysis, f, n):
     plt.show()
 
 def draw_progress(node_index, progress, total, frequency):
-    filled = int(bar_width * progress / total)
-    bar = '█' * filled + '-' * (bar_width - filled)
+    filled = int(30 * progress / total)
+    bar = '█' * filled + '-' * (30 - filled)
     sys.stdout.write(
         f'\rCase {node_index}/{len(test_cases)} [{bar}] {progress:4d}/{total} '
         f'({frequency:7.1f} kHz)'
@@ -126,26 +122,25 @@ if __name__ == '__main__':
     nodes = 4
     test_cases = list(powerset(range(1, nodes + 1)))
     #test_cases = [(), (1,), (2,), (3,), (4,)]
+    test_cases = [(1,), (2,), (1,2)]
     freq_step = 17.5 * 10
     frequencies = np.arange(1, 3500, freq_step)
     data = np.zeros((len(test_cases), len(frequencies)))
 
-    bar_width = 30
-
     for i, n in enumerate(test_cases):
         for j, f in enumerate(frequencies):
-            circuit = get_circuit(n)
+            circuit = get_circuit(n, [100@u_kOhm, 220@u_kOhm, 470@u_kOhm, 1000@u_kOhm])
 
             simulator = circuit.simulator(temperature=25, nominal_temperature=25)
 
             analysis = simulator.transient(
                 step_time=10@u_us,
-                end_time=10@u_ms
+                end_time=5@u_ms
             )
 
             #plot_all(analysis, f, n)
             venv = np.array(analysis['venv'])
-            data[i, j] = np.mean(venv[-10:])
+            data[i, j] = np.mean(venv[-5:])
 
             draw_progress(i, j + 1, len(frequencies), f)
 
@@ -167,3 +162,6 @@ if __name__ == '__main__':
     plt.grid(which='minor', linestyle=':', alpha=0.3)
     enable_legend_toggles(plt.gca(), lines)
     plt.show()
+    #plt.gcf().set_size_inches(10, 7)
+    #plt.savefig(f'{r1}_{r2}.png', dpi=1000)
+    plt.close()
